5e52b0bf2ffd6bf7b969423c0f7bef7679503dfe
Sofyan Thayf <sofyanthayf@gmail.com>
2017-12-12 21:28:13 +0800

    test email 9

--- application/controllers/Sendmail.php
+++ application/controllers/Sendmail.php
@@ -5,11 +5,23 @@ class Sendmail extends CI_Controller {
 
 	public function index()
 	{
-		// $this->email->to('sofyanthayf@gmail.com');
-		// $this->email->subject('Test email from CI and Gmail');
-		// $this->email->message('This is a test.');
-		// $this->email->send();
+		$this->email->to('sofyanthayf@gmail.com');
+		$this->email->subject('Test email from CI and Gmail');
+		$this->email->message('This is a test.');
+		$this->email->send();
 
-		$this->load->view('sendmail');
-	}
+		// $this->load->view('sendmail');
+		if ( ! $this->email->send() )	{
+			echo 'Failed';
+
+			// Loop through the debugger messages.
+			foreach ( $this->email->get_debugger_messages() as $debugger_message )
+			echo $debugger_message;
+
+			// Remove the debugger messages as they're not necessary for the next attempt.
+			$this->email->clear_debugger_messages();
+		}		else
+			echo 'Sent';
+		}
+
 }
