695aa78544df1aba14f04effed21adb8a368d27f
Sofyan Thayf <sofyanthayf@gmail.com>
2017-12-12 21:37:18 +0800

    test email 12

--- application/controllers/Sendmail.php
+++ application/controllers/Sendmail.php
@@ -13,13 +13,13 @@ class Sendmail extends CI_Controller {
 		// $this->load->view('sendmail');
 		if ( ! $this->email->send() )	{
 			echo 'Failed';
-			$this->email->print_debugger();
-			// // Loop through the debugger messages.
-			// foreach ( $this->email->get_debugger_messages() as $debugger_message )
-			// echo $debugger_message;
-      //
-			// // Remove the debugger messages as they're not necessary for the next attempt.
-			// $this->email->clear_debugger_messages();
+			
+			// Loop through the debugger messages.
+			foreach ( $this->email->get_debugger_messages() as $debugger_message )
+			echo $debugger_message;
+
+			// Remove the debugger messages as they're not necessary for the next attempt.
+			$this->email->clear_debugger_messages();
 		}		else
 			echo 'Sent';
 		}
