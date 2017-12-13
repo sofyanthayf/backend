19640ef6be28e3d05bb7180ea9909b97fb37ed32
Sofyan Thayf <sofyanthayf@gmail.com>
2017-12-13 11:06:53 +0800

    Sendmail Test 04

--- application/controllers/Sendmail.php
+++ application/controllers/Sendmail.php
@@ -12,8 +12,11 @@ class Sendmail extends REST_Controller {
 			parent::__construct();
 	}
 
-
 	function index_get(){
+		
+	}
+
+	function mail_post(){
 
 			$this->email->from('siska@kharisma.ac.id', 'SISKA');
 			$this->email->to('sofyanthayf@gmail.com');
