243a1dae7b18d5d1c4e283d83349a076b1f440f8
Sofyan Thayf <sofyanthayf@gmail.com>
2017-12-13 09:52:16 +0800

    Sendmail Test 01

--- application/controllers/Sendmail.php
+++ application/controllers/Sendmail.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
 class Sendmail extends REST_Controller {
